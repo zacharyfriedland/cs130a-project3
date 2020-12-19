bedb1b20d02bfc8953fb81e396be4bc79234ac84
zacharyfriedland <zacharyfriedland@ucsb.edu>
2020-12-18 16:08:58 -0800

    fix main

--- main.cpp
+++ main.cpp
@@ -6,94 +6,94 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-    AVL* avl = new AVL();
-    avl->insert(make_tuple(3, 5));
-    avl->insert(make_tuple(3, 9));
+//     AVL* avl = new AVL();
+//     avl->insert(make_tuple(3, 5));
+//     avl->insert(make_tuple(3, 9));
 
-    avl->insert(make_tuple(4, 5));
-    // avl->insert(make_tuple(4, 2));
-    // avl->insert(make_tuple(12, 3));
-    // avl->insert(make_tuple(1, 2));
-    // avl->insert(make_tuple(22, 2));
-    // avl->insert(make_tuple(33, 3));
-    // avl->insert(make_tuple(20, 0));
-
-
-//insert 4 5, insert 3 9, insert 3 5, insert 2 7, insert 2 6, in_order, pre_order,
-
-  avl->printInOrder();
+//     avl->insert(make_tuple(4, 5));
+//     // avl->insert(make_tuple(4, 2));
+//     // avl->insert(make_tuple(12, 3));
+//     // avl->insert(make_tuple(1, 2));
+//     // avl->insert(make_tuple(22, 2));
+//     // avl->insert(make_tuple(33, 3));
+//     // avl->insert(make_tuple(20, 0));
 
 
-  // vector<string> input;
-  // string keywords[] = {"search", "insert", "delete", "approx_search", "in_order", "pre_order", "in_order,", "pre_order,"};
-  // string argument = argv[1];
+// //insert 4 5, insert 3 9, insert 3 5, insert 2 7, insert 2 6, in_order, pre_order,
 
-  // string word;
-  // for(int i = 0; i < argument.size(); i++){
-  //   if(argument[i] == ' '){
-  //     input.push_back(word);
-  //     word = "";
-  //   }
-  //   else if(i == argument.size() - 1){
-  //     word += argument[i];
-  //     input.push_back(word);
-  //   }
-  //   else{
-  //     word += argument[i];
-  //   }
-  // }
+//   avl->printInOrder();
+
+
+  vector<string> input;
+  string keywords[] = {"search", "insert", "delete", "approx_search", "in_order", "pre_order", "in_order,", "pre_order,"};
+  string argument = argv[1];
+
+  string word;
+  for(int i = 0; i < argument.size(); i++){
+    if(argument[i] == ' '){
+      input.push_back(word);
+      word = "";
+    }
+    else if(i == argument.size() - 1){
+      word += argument[i];
+      input.push_back(word);
+    }
+    else{
+      word += argument[i];
+    }
+  }
   
-  // string h = input[0];
-  // h.erase(remove(h.begin(), h.end(), ','), h.end());
-  // int hNum = stoi(h);
-  // AVL* avl = new AVL(hNum);
+  string h = input[0];
+  h.erase(remove(h.begin(), h.end(), ','), h.end());
+  int hNum = stoi(h);
+  AVL* avl = new AVL(hNum);
 
-  // for(int i = 1; i < input.size(); i++){
-  //   if (find(begin(keywords), end(keywords), input[i]) != end(keywords)){
-  //     if(input[i] == "search"){
-  //       int param1 = stoi(input[i+1]);
-  //       string param2 = input[i+2];
-  //       param2.erase(remove(param2.begin(), param2.end(), ','), param2.end());
-  //       int param2Num = stoi(param2);
-  //       tuple<int, int> target = make_tuple(param1, param2Num);
-  //       avl->search(target);
-  //       i+=2;
-  //     }
-  //     else if(input[i] == "insert"){
-  //       int param1 = stoi(input[i+1]);
-  //       string param2 = input[i+2];
-  //       param2.erase(remove(param2.begin(), param2.end(), ','), param2.end());
-  //       int param2Num = stoi(param2);
-  //       tuple<int, int> target = make_tuple(param1, param2Num);
-  //       avl->insert(target);
-  //       i+=2;
-  //     }
-  //     else if(input[i] == "delete"){
-  //       int param1 = stoi(input[i+1]);
-  //       string param2 = input[i+2];
-  //       param2.erase(remove(param2.begin(), param2.end(), ','), param2.end());
-  //       int param2Num = stoi(param2);
-  //       tuple<int, int> target = make_tuple(param1, param2Num);
-  //       avl->remove(target);
-  //       i+=2;
-  //     }
-  //     else if(input[i] == "approx_search"){
-  //       int param1 = stoi(input[i+1]);
-  //       string param2 = input[i+2];
-  //       param2.erase(remove(param2.begin(), param2.end(), ','), param2.end());
-  //       int param2Num = stoi(param2);
-  //       tuple<int, int> target = make_tuple(param1, param2Num);
-  //       avl->approximateSearch(target);
-  //       i+=2;
-  //     }
-  //     else if(input[i] == "in_order" || input[i] == "in_order,"){
-  //       avl->printInOrder();
-  //     }
-  //     else if(input[i] == "pre_order" || input[i] == "pre_order,"){
-  //       avl->printPreOrder();
-  //     }
-  //   }
-  // }
+  for(int i = 1; i < input.size(); i++){
+    if (find(begin(keywords), end(keywords), input[i]) != end(keywords)){
+      if(input[i] == "search"){
+        int param1 = stoi(input[i+1]);
+        string param2 = input[i+2];
+        param2.erase(remove(param2.begin(), param2.end(), ','), param2.end());
+        int param2Num = stoi(param2);
+        tuple<int, int> target = make_tuple(param1, param2Num);
+        avl->search(target);
+        i+=2;
+      }
+      else if(input[i] == "insert"){
+        int param1 = stoi(input[i+1]);
+        string param2 = input[i+2];
+        param2.erase(remove(param2.begin(), param2.end(), ','), param2.end());
+        int param2Num = stoi(param2);
+        tuple<int, int> target = make_tuple(param1, param2Num);
+        avl->insert(target);
+        i+=2;
+      }
+      else if(input[i] == "delete"){
+        int param1 = stoi(input[i+1]);
+        string param2 = input[i+2];
+        param2.erase(remove(param2.begin(), param2.end(), ','), param2.end());
+        int param2Num = stoi(param2);
+        tuple<int, int> target = make_tuple(param1, param2Num);
+        avl->remove(target);
+        i+=2;
+      }
+      else if(input[i] == "approx_search"){
+        int param1 = stoi(input[i+1]);
+        string param2 = input[i+2];
+        param2.erase(remove(param2.begin(), param2.end(), ','), param2.end());
+        int param2Num = stoi(param2);
+        tuple<int, int> target = make_tuple(param1, param2Num);
+        avl->approximateSearch(target);
+        i+=2;
+      }
+      else if(input[i] == "in_order" || input[i] == "in_order,"){
+        avl->printInOrder();
+      }
+      else if(input[i] == "pre_order" || input[i] == "pre_order,"){
+        avl->printPreOrder();
+      }
+    }
+  }
   
 
   return 0;
